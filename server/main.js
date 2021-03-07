7121782fbb178f03e9316d5a51554c5567b70e12
Linus Romland <romlandlinus@gmail.com>
2021-03-07 19:02:05 +0100

    PORT CHANGED TO 3001
    Previous port 3000 changed to 3001

--- server/main.js
+++ server/main.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const dbModule = require("./dbModule");
 const Ad = require("./Models/Ad");
 const clientdir = __dirname + "/client";
